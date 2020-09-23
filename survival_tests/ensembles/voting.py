a0efc853980d53fe4c687aa629c35627a5637aee
Lukas Gehring <lgehring@mail.upb.de>
2020-09-23 10:13:35 +0200

    Differential Evolution Approach

--- survival_tests/ensembles/voting.py
+++ survival_tests/ensembles/voting.py
@@ -1,5 +1,6 @@
 import logging
 import numpy as np
+from scipy.optimize import differential_evolution
 from scipy.stats import rankdata
 
 from approaches.survival_forests.auto_surrogate import SurrogateAutoSurvivalForest
@@ -8,6 +9,7 @@ from baselines.per_algorithm_regressor import PerAlgorithmRegressor
 from baselines.satzilla11 import SATzilla11
 from baselines.sunny import SUNNY
 from aslib_scenario.aslib_scenario import ASlibScenario
+from sklearn.utils import resample
 
 from par_10_metric import Par10Metric
 
@@ -29,7 +31,7 @@ class Voting:
         self.trained_models.append(SUNNY())
         self.trained_models.append(ISAC())
         self.trained_models.append(SATzilla11())
-        self.trained_models.append(SurrogateAutoSurvivalForest())
+        #self.trained_models.append(SurrogateAutoSurvivalForest())
         self.num_models = len(self.trained_models)
 
     def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
@@ -41,13 +43,28 @@ class Voting:
         for base_learner in self.trained_models:
             base_learner.fit(scenario, fold, amount_of_training_instances)
 
-        self.weights = self.differential_evolution(scenario, fold,amount_of_training_instances)
+        bounds = [(0, 2), (0, 2), (0, 2), (0, 2)]
+        result = differential_evolution(self.validation, bounds, args=(scenario, fold, amount_of_training_instances), seed=fold, workers=4, maxiter=3, disp=True)
+        self.weights = result.x
 
-    def validation(self, scenario: ASlibScenario, amount_of_training_instances: int, weights):
-        # TODO: Add 10 fold cross validation?
+
+    def validation(self, weights, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
+        print("Call validate with weight", weights)
+        # if scenario.feature_cost_data is not None:
+        #     feature_data, performance_data, feature_cost_data = self._resample_instances(
+        #         scenario.feature_data, scenario.performance_data, scenario.feature_cost_data, amount_of_training_instances, random_state=fold)
+        # else:
+        #     feature_data, performance_data = self._resample_instances(
+        #         scenario.feature_data, scenario.performance_data, scenario.feature_cost_data,
+        #         amount_of_training_instances, random_state=fold)
+        #     feature_cost_data = None
+
         feature_data = scenario.feature_data.to_numpy()
         performance_data = scenario.performance_data.to_numpy()
         feature_cost_data = scenario.feature_cost_data.to_numpy() if scenario.feature_cost_data is not None else None
+
+        if amount_of_training_instances is -1:
+            amount_of_training_instances = len(feature_data)
 
         par10 = 0
         for instance_id in range(amount_of_training_instances):
@@ -66,54 +83,27 @@ class Voting:
                                                  scenario.algorithm_cutoff_time)
         return par10 / amount_of_training_instances
 
-    def differential_evolution(self, scenario: ASlibScenario, fold:int, amount_of_training_instances):
-        # at least 4
-        num_individuals = 10
-        min_bound = 0
-        max_bound = 5
-        diff = max_bound - min_bound
-
-        best_score = 1000000
-        optimal_weights = self.weights
-
-        #TODO: Seed
-	np.random.seed(fold)
-        population = np.random.rand(num_individuals, self.num_models)
-        population_fit_to_bounds = min_bound + population * diff
-        scaling_factor = 0.8
-        crossover = 0.5
-        generation = 0
-        while generation < 20:
-            print("Generation", generation, "started its training now")
-            for i in range(num_individuals):
-                selections = [j for j in range(num_individuals) if j != i]
-                random_selections = np.random.choice(selections, 3,
-                                                     replace=False)
-                a, b, c = population[random_selections]
-                mutant = a + scaling_factor * (b - c)
-                j_random = np.random.rand(1, 1)
-                tail = list()
-                for j in range(self.num_models):
-                    rand = np.random.rand(1, 1)
-                    if rand < crossover or rand == j_random:
-                        tail.append(mutant[j])
-                    else:
-                        tail.append(population[i][j])
-                tail_score = self.validation(scenario, amount_of_training_instances, tail)
-                pop_score = self.validation(scenario, amount_of_training_instances, population[i])
-                if tail_score < pop_score:
-                    population[i] = tail
-                    if tail_score < best_score:
-                        best_score = tail_score
-                        optimal_weights = tail
-                else:
-                    if pop_score < best_score:
-                        best_score = pop_score
-                        optimal_weights = pop_score[i]
-
-            generation = generation + 1
-
-        return optimal_weights
+    # def create_split(self, scenario: ASlibScenario, fold: int):
+    #     split_size = int(len(scenario.instances) / 10)
+    #
+    #     feature_data = scenario.feature_data.to_numpy()
+    #     performance_data = scenario.performance_data.to_numpy()
+    #
+    #     if fold is 10:
+    #         training_split_feature = feature_data[:split_size * (fold - 1):]
+    #         test_split_feature = feature_data[split_size * (fold - 1):]
+    #
+    #         training_split_performance = performance_data[:split_size * (fold - 1):]
+    #         test_split_performance = performance_data[split_size * (fold - 1):]
+    #     else:
+    #         training_split_feature = feature_data[:split_size * (fold - 1)]
+    #         training_split_feature.extend(feature_data[split_size * fold:])
+    #         test_split_feature = feature_data[split_size * (fold - 1):split_size * fold]
+    #
+    #         training_split_performance = performance_data[:split_size * (fold - 1)]
+    #         training_split_performance.extend(performance_data[split_size * fold:])
+    #         test_split_performance = performance_data[split_size * (fold - 1):split_size * fold]
+    #     return training_split_feature, test_split_feature, training_split_performance, test_split_performance
 
     def predict(self, features_of_test_instance, instance_id: int):
         predictions = np.zeros((self.num_algorithms, 1))
@@ -121,6 +111,13 @@ class Voting:
             predictions = predictions + self.weights[i] * rankdata(model.predict(features_of_test_instance, instance_id)).reshape(
                 self.num_algorithms, 1)
         return predictions / self.num_models
+
+    def _resample_instances(self, feature_data, performance_data, feature_cost_data, num_instances, random_state):
+        num_instances = min(num_instances, np.size(
+            performance_data, axis=0)) if num_instances > 0 else np.size(performance_data, axis=0)
+        if feature_cost_data is not None:
+            return resample(feature_data, performance_data, feature_cost_data, n_samples=num_instances, random_state=random_state)
+        return resample(feature_data, performance_data, n_samples=num_instances, random_state=random_state)
 
     def get_name(self):
         return "voting"
