a4b9d9eb778a00c617a50d0a3f7a4d725e1fe98e
Lukas Gehring <lgehring@mail.upb.de>
2020-10-09 12:24:15 +0200

    Stacking plot

---
+++ survival_tests/plots/stacking_plot.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import configparser
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def load_configuration():
+    config = configparser.ConfigParser()
+    config.read_file(open('../conf/experiment_configuration.cfg'))
+    return config
+
+
+def generate_sbs_vbs_change_table():
+    stacking = get_dataframe_for_sql_query(
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking.approach, vbs_sbs.metric, stacking.result, ((stacking.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking ON vbs_sbs.scenario_name = stacking.scenario_name AND vbs_sbs.fold = stacking.fold AND vbs_sbs.metric = stacking.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
+    stacking_cross_validation = get_dataframe_for_sql_query(
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking.approach, vbs_sbs.metric, stacking.result, ((stacking.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking ON vbs_sbs.scenario_name = stacking.scenario_name AND vbs_sbs.fold = stacking.fold AND vbs_sbs.metric = stacking.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking_cross_validation' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
+
+    plt.axhline(np.average(stacking.result), color='b', linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(stacking_cross_validation.result), color='k', linestyle='dashed', linewidth=1)
+
+    plt.plot(stacking.scenario_name, stacking.result)
+    plt.plot(stacking_cross_validation.scenario_name, stacking_cross_validation.result)
+    #plt.axis([0, 24, 0, 50])
+    plt.xticks(rotation=90)
+    plt.show()
+
+
+def get_dataframe_for_sql_query(sql_query: str):
+    db_credentials = get_database_credential_string()
+    return pd.read_sql(sql_query, con=db_credentials)
+
+
+def get_database_credential_string():
+    config = load_configuration()
+    db_config_section = config['DATABASE']
+    db_host = db_config_section['host']
+    db_username = db_config_section['username']
+    db_password = db_config_section['password']
+    db_database = db_config_section['database']
+    return "mysql://" + db_username + ":" + db_password + "@" + db_host + "/" + db_database
+
+
+config = load_configuration()
+generate_sbs_vbs_change_table()
