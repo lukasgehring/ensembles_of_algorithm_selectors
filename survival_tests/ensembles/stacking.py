a204877f852eb85b9279db158bc6e5d4faa6ee60
Lukas Gehring <lgehring@mail.upb.de>
2020-10-23 12:34:23 +0200

    Update stacking

--- survival_tests/ensembles/stacking.py
+++ survival_tests/ensembles/stacking.py
@@ -1,6 +1,4 @@
 import logging
-import sys
-
 import numpy as np
 from approaches.survival_forests.surrogate import SurrogateSurvivalForest
 from baselines.isac import ISAC
@@ -17,66 +15,72 @@ class Stacking:
     def __init__(self, cross_validation=False, feature_selection=None):
         self.logger = logging.getLogger("stacking")
         self.logger.addHandler(logging.StreamHandler())
-        self.meta_learner = None
-        self.base_learners = list()
+
+        # parameters
         self.cross_validation = cross_validation
         self.feature_selection = feature_selection
 
-        self.num_models = 0
+        # attributes
+        self.meta_learner = None
+        self.base_learners = list()
+        self.num_algorithms = 0
 
     def create_base_learner(self):
         self.base_learners.append(PerAlgorithmRegressor())
         self.base_learners.append(SUNNY())
         self.base_learners.append(ISAC())
-        #self.base_learners.append(SATzilla11())
-        #self.base_learners.append(MultiClassAlgorithmSelector())
-        #self.base_learners.append(SurrogateSurvivalForest(criterion='Exponential'))
-        #self.base_learners.append(SurrogateSurvivalForest(criterion='PAR10'))
+        self.base_learners.append(SATzilla11())
+        self.base_learners.append(MultiClassAlgorithmSelector())
+        self.base_learners.append(SurrogateSurvivalForest(criterion='Exponential'))
+        self.base_learners.append(SurrogateSurvivalForest(criterion='PAR10'))
 
     def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
-        print("Start training on fold:", fold)
-        num_instances = len(scenario.instances)
-        np.random.seed(seed=fold)
-        index_array = np.random.choice(num_instances, num_instances, replace=False)
-
+        # setup
         self.create_base_learner()
-
+        self.num_algorithms = len(scenario.algorithms)
+        num_instances = len(scenario.instances)
         feature_data = scenario.feature_data.to_numpy()
-        new_feature_data = np.zeros((len(scenario.instances), len(self.base_learners)))
+        new_feature_data = np.zeros(num_instances, self.num_algorithms)
+
         instance_counter = 0
+
+        # With or without 10-fold cross validation
         if self.cross_validation:
             for sub_fold in range(1, 11):
-                test_scenario, training_scenario = self.split_scenario(scenario, sub_fold, num_instances, index_array)
+                test_scenario, training_scenario = self.split_scenario(scenario, sub_fold, num_instances)
 
-                # train base learner
                 for learner_index, base_learner in enumerate(self.base_learners):
-                    print("Start training of base learner on subfold", sub_fold)
+                    # train base learner
                     base_learner.fit(training_scenario, fold, amount_of_training_instances)
 
+                    # create new feature data
                     for instance_number in range(instance_counter, instance_counter + len(test_scenario.instances)):
                         prediction = base_learner.predict(feature_data[instance_number], instance_number)
-                        new_feature_data[instance_number][learner_index] = np.argmin(prediction)
+                        new_feature_data[instance_number][np.argmin(prediction)] = new_feature_data[instance_number][np.argmin(prediction)] + 1
+
                 instance_counter = instance_counter + len(test_scenario.instances)
         else:
-            # train base learner
             for learner_index, base_learner in enumerate(self.base_learners):
+                # train base learner
                 base_learner.fit(scenario, fold, amount_of_training_instances)
+
+                # create new feature data
                 for instance_number in range(amount_of_training_instances):
                     prediction = base_learner.predict(feature_data[instance_number], instance_number)
-                    new_feature_data[instance_number][learner_index] = np.argmin(prediction)
+                    new_feature_data[instance_number][np.argmin(prediction)] = new_feature_data[instance_number][np.argmin(prediction)] + 1
 
         # add predictions to the features of the instances
         new_feature_data = np.concatenate((feature_data, new_feature_data), axis=1)
         scenario.feature_data = new_feature_data
 
-        # meta learner training
-        print("Start training of the meta-learner")
+        # meta learner training with or without feature selection
         self.meta_learner = PerAlgorithmRegressor(feature_selection=self.feature_selection)
         self.meta_learner.fit(scenario, fold, amount_of_training_instances)
-        #sys.exit("Ende2")
 
-    def split_scenario(self, scenario: ASlibScenario, sub_fold: int, num_instances: int, index):
+    def split_scenario(self, scenario: ASlibScenario, sub_fold: int, num_instances: int):
         fold_len = int(num_instances / 10)
+
+        # TODO: Do I need this? ----------------
         instances = scenario.instances
         if sub_fold < 10:
             test_insts = instances[(sub_fold - 1) * fold_len:sub_fold * fold_len]
@@ -85,6 +89,7 @@ class Stacking:
         else:
             test_insts = instances[(sub_fold - 1) * fold_len:]
             training_insts = instances[:(sub_fold - 1) * fold_len]
+        # TODO: --------------------------------
 
         test = copy.copy(scenario)
         training = copy.copy(scenario)
@@ -132,16 +137,14 @@ class Stacking:
 
 
     def predict(self, features_of_test_instance, instance_id: int):
-        print("Predict instance", instance_id)
         # get all predictions from the base learners
-        base_learner_predictions = list()
-        for base_learner in self.base_learners:
-            base_learner_predictions.append(base_learner.predict(features_of_test_instance, instance_id))
+        new_feature_data = np.zeros(self.num_algorithms)
 
-        # add the predictions to the features of the instance
-        new_feature_data = np.zeros(len(self.base_learners))
-        for i in range(len(self.base_learners)):
-            new_feature_data[i] = np.argmin(base_learner_predictions[i])
+        for base_learner in self.base_learners:
+            # create new feature data
+            prediction = base_learner.predict(features_of_test_instance, instance_id)
+            new_feature_data[np.argmin(prediction)] = new_feature_data[np.argmin(prediction)] + 1
+
         features_of_test_instance = np.concatenate((features_of_test_instance, new_feature_data), axis=0)
 
         # final prediction
