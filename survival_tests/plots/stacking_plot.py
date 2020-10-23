d4b357ac17899851d7673d65177aca954ed6d129
Lukas Gehring <lgehring@mail.upb.de>
2020-10-23 10:45:52 +0200

    Add plots

--- survival_tests/plots/stacking_plot.py
+++ survival_tests/plots/stacking_plot.py
@@ -11,17 +11,40 @@ def load_configuration():
 
 
 def generate_sbs_vbs_change_table():
+    color1 = '#2a9d8f'
+    color2 = '#e9c46a'
+    color3 = '#e76f51'
+    color4 = '#264653'
+
     stacking = get_dataframe_for_sql_query(
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking.approach, vbs_sbs.metric, stacking.result, ((stacking.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking ON vbs_sbs.scenario_name = stacking.scenario_name AND vbs_sbs.fold = stacking.fold AND vbs_sbs.metric = stacking.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
     stacking_cross_validation = get_dataframe_for_sql_query(
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking.approach, vbs_sbs.metric, stacking.result, ((stacking.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking ON vbs_sbs.scenario_name = stacking.scenario_name AND vbs_sbs.fold = stacking.fold AND vbs_sbs.metric = stacking.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking_cross_validation' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
-    plt.axhline(np.average(stacking.result), color='b', linestyle='dashed', linewidth=1)
-    plt.axhline(np.average(stacking_cross_validation.result), color='k', linestyle='dashed', linewidth=1)
+    stacking_variance_threshold = get_dataframe_for_sql_query(
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking.approach, vbs_sbs.metric, stacking.result, ((stacking.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking ON vbs_sbs.scenario_name = stacking.scenario_name AND vbs_sbs.fold = stacking.fold AND vbs_sbs.metric = stacking.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking_VarianceThreshold' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
 
-    plt.plot(stacking.scenario_name, stacking.result)
-    plt.plot(stacking_cross_validation.scenario_name, stacking_cross_validation.result)
-    #plt.axis([0, 24, 0, 50])
+    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+
+    plt.axhline(np.average(stacking.result), color=color1, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(stacking_cross_validation.result), color=color2, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(stacking_variance_threshold.result), color=color3, linestyle='dashed', linewidth=1)
+
+    width = 0.2  # the width of the bars
+    ind = np.arange(len(stacking.scenario_name))
+    ax.bar(ind - width / 2, stacking.result, width, color=color1, label='Stacking')
+    ax.bar(ind + width / 2, stacking_cross_validation.result, width, color=color2, label='Stacking with cross validation')
+
+    ind = np.arange(len(stacking_variance_threshold.scenario_name))
+    ax.bar(ind + 3 * width / 2, stacking_variance_threshold.result, width, color=color3, label='Stacking with feature selection')
+
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(stacking_variance_threshold.scenario_name)
+
+    #ax.plot(stacking.scenario_name, stacking.result, color=stacking_color, label='Stacking')
+    #ax.plot(voting.scenario_name, voting.result, color=voting_color, label='Voting')
+    #ax.axis([0, 24, 0, 50])
     plt.xticks(rotation=90)
+    plt.legend()
     plt.show()
 
 
