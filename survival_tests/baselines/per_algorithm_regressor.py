ee7b21d74d06d6d4b0c68d8dd08e09bb7bd9a941
Lukas Gehring <lgehring@mail.upb.de>
2020-10-26 12:55:33 +0100

    Update per algorithm regressor for feature selection

--- survival_tests/baselines/per_algorithm_regressor.py
+++ survival_tests/baselines/per_algorithm_regressor.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.utils import resample
 from sklearn.base import clone
+
+from par_10_metric import Par10Metric
 from .utils import impute_censored, distr_func
 from aslib_scenario.aslib_scenario import ASlibScenario
 from sklearn.pipeline import Pipeline
@@ -19,43 +21,47 @@ class PerAlgorithmRegressor:
         self.scikit_regressor = scikit_regressor
         self.logger = logging.getLogger("per_algorithm_regressor")
         self.logger.addHandler(logging.StreamHandler())
+
+        # attributes
         self.trained_models = list()
-        self.trained_imputers = list()
-        self.trained_scalers = list()
         self.trained_pipes = list()
         self.num_algorithms = 0
         self.algorithm_cutoff_time = -1
+        self.metric = Par10Metric()
+
+        # setup
         self.impute_censored = impute_censored
         self.feature_selection = feature_selection
         self.data_weights = data_weights
         self.stump = stump
 
     def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
-        #print("Run fit on " + self.get_name() + " for fold " + str(fold))
+        # set attributes
         self.num_algorithms = len(scenario.algorithms)
         self.algorithm_cutoff_time = scenario.algorithm_cutoff_time
 
+        # train a regressor function per algorithm
         for algorithm_id in range(self.num_algorithms):
             X_train, y_train = self.get_x_y(
                 scenario, amount_of_training_instances, algorithm_id, fold)
 
-            # pipeline
+            # pipeline selection - with or without feature selection
             if self.feature_selection is None:
                 pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler())])
             elif self.feature_selection == 'VarianceThreshold':
                 pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler()), ('VarianceThreshold', VarianceThreshold(threshold=(.8 * (1 - .8))))])
             elif self.feature_selection == 'SelectKBest_f_regression':
-                optimal_feature_number = self.calculate_optimal_feature_number('SelectKBest_f_regression', X_train, y_train)
+                optimal_feature_number = self.calculate_optimal_feature_number('SelectKBest_f_regression', X_train, y_train, fold, scenario)
                 pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler()), ('SelectKBest_f_regression', SelectKBest(f_regression, k=optimal_feature_number))])
             elif self.feature_selection == 'SelectKBest_mutual_info_regression':
-                optimal_feature_number = self.calculate_optimal_feature_number('SelectKBest_mutual_info_regression', X_train, y_train, fold)
-                print("Create new final Pipe")
-                #sys.exit("Ende")
+                optimal_feature_number = self.calculate_optimal_feature_number('SelectKBest_mutual_info_regression', X_train, y_train, fold, scenario)
                 pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler()), ('SelectKBest_mutual_info_regression', SelectKBest(mutual_info_regression, k=optimal_feature_number))])
 
+            # apply the pipeline
             X_train = pipe.fit_transform(X_train, y_train)
             self.trained_pipes.append(pipe)
 
+            # train the model (with/without stump/weight)
             model = clone(self.scikit_regressor)
             if self.stump:
                 model.set_params(random_state=fold, max_depth=1)
@@ -66,7 +72,6 @@ class PerAlgorithmRegressor:
                 censored = y_train >= self.algorithm_cutoff_time
                 model = impute_censored(
                     X_train, y_train, censored, model, distr_func, self.algorithm_cutoff_time)
-
             else:
                 if self.data_weights is None:
                     model.fit(X_train, y_train)
@@ -82,6 +87,7 @@ class PerAlgorithmRegressor:
             X_test = np.reshape(features_of_test_instance,
                                 (1, len(features_of_test_instance)))
 
+            # prepare features for prediction
             X_test = self.trained_pipes[algorithm_id].transform(X_test)
 
             model = self.trained_models[algorithm_id]
@@ -91,12 +97,27 @@ class PerAlgorithmRegressor:
 
         return np.asarray(predicted_risk_scores)
 
-    def calculate_optimal_feature_number(self, feature_selector, X_data, y_data, fold:int):
+    def calculate_optimal_feature_number(self, feature_selector, X_data, y_data, fold: int, scenario: ASlibScenario):
+        # create a 9/10 train - 1/10 test split
         X_train, y_train, X_test, y_test = self.create_fold(1, X_data, y_data)
-        best_score = 0
+        best_score = 1000000
+        feature_number = len(X_train[0])
         optimal_number = 1
-        for number_of_features in range(1, len(X_train[0])):
-            pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler()), ('SelectKBest_mutual_info_regression', SelectKBest(mutual_info_regression, k=number_of_features))])
+        feature_cost_data = scenario.feature_cost_data.to_numpy() if scenario.feature_cost_data is not None else None
+
+        # add 10 or 11 steps for the optimal feature number
+        if feature_number > 9:
+            steps = np.arange(start=1, stop=feature_number, step=int(feature_number / 10))
+        else:
+            steps = np.arange(start=1, stop=feature_number, step=1)
+
+        # train and test the feature numbers with the split
+        for number_of_features in steps:
+            if feature_selector == 'SelectKBest_f_regression':
+                pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler()), ('SelectKBest_f_regression', SelectKBest(f_regression, k=number_of_features))])
+            else:
+                pipe = Pipeline([('imputer', SimpleImputer()), ('standard_scaler', StandardScaler()), ('SelectKBest_mutual_info_regression', SelectKBest(mutual_info_regression, k=number_of_features))])
+
             X_train_validation = pipe.fit_transform(X_train, y_train)
             X_test_validation = pipe.transform(X_test)
 
@@ -104,15 +125,22 @@ class PerAlgorithmRegressor:
             model.set_params(random_state=fold)
             model.fit(X_train_validation, y_train)
 
-            score = 0
-            for i, x_test in enumerate(X_test_validation):
-                prediction = model.predict(x_test.reshape(1, -1))
-                if prediction == y_test[i]:
-                    score = score + 1
-            if best_score < score:
-                best_score = score
+            performance_measure = 0
+            for instance_id, x_test in enumerate(X_test_validation):
+                # TODO: Only works for fold 1!
+                accumulated_feature_time = 0
+                if scenario.feature_cost_data is not None:
+                    feature_time = feature_cost_data[instance_id]
+                    accumulated_feature_time = np.sum(feature_time)
+
+                predicted_scores = model.predict(x_test.reshape(1, -1))
+                performance_measure = performance_measure + self.metric.evaluate(y_test, predicted_scores,
+                                                                                 accumulated_feature_time,
+                                                                                 scenario.algorithm_cutoff_time)
+            if best_score > performance_measure:
+                best_score = performance_measure
                 optimal_number = number_of_features
-            print("Score", score, "with", number_of_features, "features.")
+            ("Score", performance_measure, "with", number_of_features, "features.")
 
         return optimal_number
 
@@ -124,7 +152,7 @@ class PerAlgorithmRegressor:
 
             X_train = X_data[fold * fold_lenght:]
             y_train = y_data[fold * fold_lenght:]
-        elif fold == 11:
+        elif fold == 10:
             X_test = X_data[(fold - 1) * fold_lenght:]
             y_test = y_data[(fold - 1) * fold_lenght:]
 
@@ -177,7 +205,8 @@ class PerAlgorithmRegressor:
         name = ''
         if self.impute_censored:
             name += 'imputed_'
-
         name += 'per_algorithm_{}_regressor'.format(
             type(self.scikit_regressor).__name__)
+        if self.feature_selection is not None:
+            name += self.feature_selection
         return name
