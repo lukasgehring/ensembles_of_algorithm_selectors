22c3f69917be5379cee175abbc3e0c7c9e586cd9
Lukas Gehring <lgehring@mail.upb.de>
2020-11-19 11:18:46 +0100

    Add plots

--- survival_tests/plots/stacking_plot.py
+++ survival_tests/plots/stacking_plot.py
@@ -25,21 +25,26 @@ def generate_sbs_vbs_change_table():
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking_new.approach, vbs_sbs.metric, stacking_new.result, ((stacking_new.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking_new ON vbs_sbs.scenario_name = stacking_new.scenario_name AND vbs_sbs.fold = stacking_new.fold AND vbs_sbs.metric = stacking_new.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking_SelectKBest_f_regression' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
     stacking_mutual = get_dataframe_for_sql_query(
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking_new.approach, vbs_sbs.metric, stacking_new.result, ((stacking_new.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking_new ON vbs_sbs.scenario_name = stacking_new.scenario_name AND vbs_sbs.fold = stacking_new.fold AND vbs_sbs.metric = stacking_new.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking_SelectKBest_mutual_info_regression' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
+    stacking_after_fix = get_dataframe_for_sql_query(
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, stacking_after_fix.approach, vbs_sbs.metric, stacking_after_fix.result, ((stacking_after_fix.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN stacking_after_fix ON vbs_sbs.scenario_name = stacking_after_fix.scenario_name AND vbs_sbs.fold = stacking_after_fix.fold AND vbs_sbs.metric = stacking_after_fix.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='stacking_per_algorithm_regressor' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
 
     fig, ax = plt.subplots()  # Create a figure containing a single axes.
 
     plt.axhline(np.average(stacking.result), color=color1, linestyle='dashed', linewidth=1)
     plt.axhline(np.average(stacking_variance_threshold.result), color=color2, linestyle='dashed', linewidth=1)
-    plt.axhline(np.average(stacking_regression_new.result), color=color3, linestyle='dashed', linewidth=1)
-    plt.axhline(np.average(stacking_mutual.result), color=color4, linestyle='dashed', linewidth=1)
+    #plt.axhline(np.average(stacking_regression_new.result), color=color3, linestyle='dashed', linewidth=1)
+    #plt.axhline(np.average(stacking_mutual.result), color=color4, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(stacking_after_fix.result), color=color5, linestyle='dashed', linewidth=1)
 
-    width = 0.2  # the width of the bars
+    width = 0.25  # the width of the bars
     ind = np.arange(len(stacking.scenario_name))
-    ax.bar(ind - width / 2, stacking.result, width, color=color1, label='Stacking')
-    ax.bar(ind + width / 2, stacking_variance_threshold.result, width, color=color2, label='Stacking Variance Threshold')
-    ax.bar(ind + 3 * width / 2, stacking_regression_new.result, width, color=color3,
-           label='Stacking SelectKBest f_regression')
-    ax.bar(ind + 5 * width / 2, stacking_mutual.result, width, color=color4, label='Stacking SelectKBest mutual_info_regression')
+    ax.bar(ind - 3 * width / 2, stacking.result, width, color=color1, label='Stacking')
+    ax.bar(ind - width / 2, stacking_variance_threshold.result, width, color=color2, label='Stacking Variance Threshold')
+    #ax.bar(ind + width / 2, stacking_regression_new.result, width, color=color3,
+    #       label='Stacking SelectKBest f_regression')
+    #ax.bar(ind + 3 * width / 2, stacking_mutual.result, width, color=color4, label='Stacking SelectKBest mutual_info_regression')
+    ax.bar(ind + width / 2, stacking_after_fix.result, width, color=color5,
+           label='Stacking after fix')
 
     ax.set_xticks(ind + width / 2)
     ax.set_xticklabels(stacking.scenario_name)
--- survival_tests/plots/voting_plot.py
+++ survival_tests/plots/voting_plot.py
@@ -11,9 +11,11 @@ def load_configuration():
 
 
 def generate_sbs_vbs_change_table():
-    voting_color = '#2a9d8f'
-    voting_weighting_color = '#e9c46a'
-    voting_weighting_cross_color = '#264653'
+    color1 = '#2a9d8f'
+    color2 = '#e9c46a'
+    color3 = '#e76f51'
+    color4 = '#264653'
+    color5 = '#251314'
 
     voting = get_dataframe_for_sql_query(
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, voting.approach, vbs_sbs.metric, voting.result, ((voting.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN voting ON vbs_sbs.scenario_name = voting.scenario_name AND vbs_sbs.fold = voting.fold AND vbs_sbs.metric = voting.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='voting' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
@@ -21,30 +23,31 @@ def generate_sbs_vbs_change_table():
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, voting_weighting.approach, vbs_sbs.metric, voting_weighting.result, ((voting_weighting.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN voting_weighting ON vbs_sbs.scenario_name = voting_weighting.scenario_name AND vbs_sbs.fold = voting_weighting.fold AND vbs_sbs.metric = voting_weighting.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
     voting_weighting_cross = get_dataframe_for_sql_query(
         "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, voting_weighting_cross.approach, vbs_sbs.metric, voting_weighting_cross.result, ((voting_weighting_cross.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN voting_weighting_cross ON vbs_sbs.scenario_name = voting_weighting_cross.scenario_name AND vbs_sbs.fold = voting_weighting_cross.fold AND vbs_sbs.metric = voting_weighting_cross.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
+    voting_after_fix = get_dataframe_for_sql_query(
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, voting_cross_validation_fix.approach, vbs_sbs.metric, voting_cross_validation_fix.result, ((voting_cross_validation_fix.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN voting_cross_validation_fix ON vbs_sbs.scenario_name = voting_cross_validation_fix.scenario_name AND vbs_sbs.fold = voting_cross_validation_fix.fold AND vbs_sbs.metric = voting_cross_validation_fix.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
 
-    print(voting_weighting)
-
-    print("")
-
-    print(voting_weighting_cross)
 
     fig, ax = plt.subplots()  # Create a figure containing a single axes.
 
-    plt.axhline(np.average(voting_weighting.result), color=voting_weighting_color, linestyle='dashed', linewidth=1)
-    plt.axhline(np.average(voting.result), color=voting_color, linestyle='dashed', linewidth=1)
-    plt.axhline(np.average(voting_weighting_cross.result), color=voting_weighting_cross_color, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(voting_weighting.result), color=color1, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(voting.result), color=color2, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(voting_weighting_cross.result), color=color3, linestyle='dashed', linewidth=1)
+    plt.axhline(np.average(voting_after_fix.result), color=color4, linestyle='dashed',
+                linewidth=1)
 
     width = 0.2  # the width of the bars
     ind = np.arange(len(voting_weighting_cross.scenario_name))
-    ax.bar(ind + 4 * width / 3, voting_weighting_cross.result, width, color=voting_weighting_cross_color,
+    ax.bar(ind - 3 * width / 3, voting_weighting_cross.result, width, color=color3,
            label='Voting Weighting with Cross Validation')
 
-    ind = np.arange(len(voting_weighting.scenario_name))
-    ax.bar(ind - width / 2, voting_weighting.result, width, color=voting_weighting_color, label='Voting Weighting')
-    ax.bar(ind + width / 2, voting.result, width, color=voting_color, label='Voting')
+    ax.bar(ind - width / 2, voting_weighting.result, width, color=color1, label='Voting Weighting')
+    ax.bar(ind + width / 2, voting.result, width, color=color2, label='Voting')
+
+    ind = np.arange(len(voting_after_fix.scenario_name))
+    ax.bar(ind + 3 * width / 2, voting_after_fix.result, width, color=color4, label='Voting after fix')
 
     ax.set_xticks(ind + width / 2)
-    ax.set_xticklabels(voting_weighting.scenario_name)
+    ax.set_xticklabels(voting_after_fix.scenario_name)
 
     #ax.plot(stacking.scenario_name, stacking.result, color=stacking_color, label='Stacking')
     #ax.plot(voting.scenario_name, voting.result, color=voting_color, label='Voting')
