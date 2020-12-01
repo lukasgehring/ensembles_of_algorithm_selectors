414a0e63820b433d2027ffb7bc946b96be380aa3
Lukas Gehring <lgehring@mail.upb.de>
2020-12-01 11:37:10 +0100

    Add out of sample weighting for bagging

--- survival_tests/ensembles/bagging.py
+++ survival_tests/ensembles/bagging.py
@@ -1,6 +1,7 @@
 import copy
 import logging
 import numpy as np
+import sys
 
 from baselines.per_algorithm_regressor import PerAlgorithmRegressor
 from aslib_scenario.aslib_scenario import ASlibScenario
@@ -28,6 +29,7 @@ class Bagging:
     # generate number_of_samples bootstrap samples from the scenario and returns them in a list
     def generate_bootstrap_sample(self, scenario: ASlibScenario, fold: int, number_of_samples: int):
         bootstrap_samples = list()
+        out_of_sample_samples = list()
         sample_size = len(scenario.instances)
         random_seed = (fold - 1) * number_of_samples
 
@@ -42,16 +44,41 @@ class Bagging:
                 feature_cost_data_sample = None
 
             bootstrap_samples.append((feature_data_sample, performance_data_sample, runstatus_data_sample, feature_runstatus_data_sample, feature_cost_data_sample))
+            if self.weighting:
+                out_of_sample_samples.append(self.get_out_of_sample(scenario, feature_data_sample.drop_duplicates()))
+
             random_seed = random_seed + 1
 
-        return bootstrap_samples
+        return bootstrap_samples, out_of_sample_samples
+
+    def get_out_of_sample(self, scenario: ASlibScenario, feature_data_sample):
+        out_of_sample = list()
+        for instance in scenario.feature_data.index:
+            is_in = False
+            for index in feature_data_sample.index:
+                if instance == index:
+                    is_in = True
+                    break
+            if not is_in:
+                out_of_sample.append(instance)
+
+        out_of_sample_feature_data = scenario.feature_data.loc[out_of_sample]
+        out_of_sample_performance_data = scenario.performance_data.loc[out_of_sample]
+        out_of_sample_runstatus_data = scenario.runstatus_data.loc[out_of_sample]
+        out_of_sample_feature_runstatus_data = scenario.feature_runstatus_data.loc[out_of_sample]
+        if scenario.feature_cost_data is not None:
+            out_of_sample_feature_cost_data = scenario.feature_cost_data.loc[out_of_sample]
+        else:
+            out_of_sample_feature_cost_data = None
+
+        return (out_of_sample_feature_data, out_of_sample_performance_data, out_of_sample_runstatus_data, out_of_sample_feature_runstatus_data, out_of_sample_feature_cost_data)
 
     def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
         print("Run fit on " + self.get_name() + " for fold " + str(fold))
         self.num_algorithms = len(scenario.algorithms)
 
         # create all bootstrap samples
-        bootstrap_samples = self.generate_bootstrap_sample(scenario, fold, self.num_base_learner)
+        bootstrap_samples, out_of_sample_samples = self.generate_bootstrap_sample(scenario, fold, self.num_base_learner)
 
         weights_denorm = list()
 
@@ -61,7 +88,8 @@ class Bagging:
             scenario.feature_data, scenario.performance_data, scenario.runstatus_data, scenario.feature_runstatus_data, scenario.feature_cost_data = bootstrap_samples[index]
             self.base_learners[index].fit(scenario, fold, amount_of_training_instances)
             if self.weighting:
-                weights_denorm.append(base_learner_performance(scenario, amount_of_training_instances, self.base_learners[index]))
+                scenario.feature_data, scenario.performance_data, scenario.runstatus_data, scenario.feature_runstatus_data, scenario.feature_cost_data = out_of_sample_samples[index]
+                weights_denorm.append(base_learner_performance(scenario, len(scenario.feature_data), self.base_learners[index]))
 
         # Turn around values (lowest (best) gets highest weight) and normalize
         weights_denorm = [max(weights_denorm) / float(i + 1) for i in weights_denorm]
@@ -95,6 +123,6 @@ class Bagging:
             name = name + "_without_ranking"
 
         if self.weighting:
-            name = name + "_weighting"
+            name = name + "_weighting_oos"
 
         return name
