37d97437131f9995aba2bc37b6e884eeeef4a6c7
Lukas Gehring <lgehring@mail.upb.de>
2020-12-03 10:27:49 +0100

    Add rank_method option

--- survival_tests/ensembles/prediction.py
+++ survival_tests/ensembles/prediction.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.stats import rankdata
 
-def predict_with_ranking(features_of_test_instance, instance_id: int, num_algorithms: int, trained_models, weights=None, performance_rank=False):
+
+def predict_with_ranking(features_of_test_instance, instance_id: int, num_algorithms: int, trained_models, weights=None, performance_rank=False, rank_method='average'):
     # use all predictions (ranking) of the base learner
     predictions = np.zeros((num_algorithms, 1))
     for i, model in enumerate(trained_models):
@@ -12,11 +13,11 @@ def predict_with_ranking(features_of_test_instance, instance_id: int, num_algori
             if performance_rank:
                 predictions = predictions.reshape(num_algorithms) + weights[i] * (prediction / sum(prediction)).reshape(num_algorithms)
             else:
-                predictions = predictions.reshape(num_algorithms) + weights[i] * rankdata(prediction).reshape(num_algorithms)
+                predictions = predictions.reshape(num_algorithms) + weights[i] * rankdata(prediction, method=rank_method).reshape(num_algorithms)
         else:
             if performance_rank:
                 predictions = predictions.reshape(num_algorithms) + (prediction / sum(prediction)).reshape(num_algorithms)
             else:
-                predictions = predictions.reshape(num_algorithms) + rankdata(prediction)
+                predictions = predictions.reshape(num_algorithms) + rankdata(prediction, method=rank_method)
 
     return predictions / sum(predictions)
