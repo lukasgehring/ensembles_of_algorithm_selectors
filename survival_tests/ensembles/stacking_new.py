5ec9d6588601d8d243945c83ec0fe1772a359a37
Lukas Gehring <lgehring@mail.upb.de>
2020-12-12 13:15:34 +0100

    Add name change for stacking

--- survival_tests/ensembles/stacking_new.py
+++ survival_tests/ensembles/stacking_new.py
@@ -1,6 +1,6 @@
 import logging
 import numpy as np
-import pandas as pd
+import sys
 from sklearn.ensemble import RandomForestClassifier
 
 from approaches.survival_forests.surrogate import SurrogateSurvivalForest
@@ -18,7 +18,7 @@ from ensembles.validation import base_learner_performance
 
 class StackingNew:
 
-    def __init__(self, meta_learner_type='per_algorithm_regressor', cross_validation=False, feature_selection=None, base_learner=None):
+    def __init__(self, meta_learner_type='random_forest_classifier', cross_validation=False, feature_selection=None, base_learner=None, type='standard'):
         self.logger = logging.getLogger("stacking")
         self.logger.addHandler(logging.StreamHandler())
 
@@ -27,7 +27,7 @@ class StackingNew:
         self.feature_selection = feature_selection
         self.meta_learner_type = meta_learner_type
         self.base_learner = base_learner
-
+        self.type = type
 
         # attributes
         self.meta_learner = None
@@ -52,27 +52,40 @@ class StackingNew:
         if 7 in self.base_learner:
             self.base_learners.append(MultiClassAlgorithmSelector())
 
-    def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
+    def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int, pre_computed=False):
         # setup
-        self.create_base_learner()
+        if not pre_computed:
+            self.create_base_learner()
         self.num_algorithms = len(scenario.algorithms)
         feature_data = scenario.feature_data.to_numpy()
         num_instances = len(feature_data)
-        x_train = np.zeros((num_instances, self.num_algorithms))
+
+        if self.type == 'standard':
+            x_train = np.zeros((num_instances, self.num_algorithms))
+        elif self.type == 'full_prediction':
+            x_train = [[] for x in range(num_instances)]
 
         for i, base_learner in enumerate(self.base_learners):
-            base_learner.fit(scenario, fold, amount_of_training_instances)
+            if not pre_computed:
+                base_learner.fit(scenario, fold, amount_of_training_instances)
             for instance_number, x_test in enumerate(feature_data):
-                algorithm_prediction = np.argmin(base_learner.predict(x_test, instance_number))
-                x_train[instance_number][algorithm_prediction] = x_train[instance_number][algorithm_prediction] + 1
+                algorithm_prediction = base_learner.predict(x_test, instance_number)
+                if self.type == 'standard':
+                    algorithm_prediction = np.argmin(algorithm_prediction)
+                    x_train[instance_number][algorithm_prediction] = x_train[instance_number][algorithm_prediction] + 1
+                elif self.type == 'full_prediction':
+                    # TODO: not finished
+                    x_train[instance_number].extend(algorithm_prediction)
 
-        self.meta_learner = RandomForestClassifier(n_jobs=1, n_estimators=100)
+        if self.meta_learner_type == 'random_forest_classifier':
+            self.meta_learner = RandomForestClassifier(n_jobs=1, n_estimators=100)
+        else:
+            sys.exit("Wrong meta learner type")
         self.meta_learner.set_params(random_state=fold)
         y_train = list()
         for data in scenario.performance_data.to_numpy():
             y_train.append(np.argmin(data))
         self.meta_learner.fit(x_train, y_train)
-
 
     def predict(self, features_of_test_instance, instance_id: int):
         # get all predictions from the base learners
@@ -80,8 +93,12 @@ class StackingNew:
 
         for base_learner in self.base_learners:
             # create new feature data
-            algorithm_prediction = np.argmin(base_learner.predict(features_of_test_instance, instance_id))
-            new_feature_data[algorithm_prediction] = new_feature_data[algorithm_prediction] + 1
+            algorithm_prediction = base_learner.predict(features_of_test_instance, instance_id)
+            if self.type == 'standard':
+                algorithm_prediction = np.argmin(algorithm_prediction)
+                new_feature_data[algorithm_prediction] = new_feature_data[algorithm_prediction] + 1
+            elif self.type == 'full_prediction':
+                new_feature_data.extend(algorithm_prediction)
 
         features_of_test_instance = new_feature_data.reshape(1, -1)
 
@@ -93,7 +110,7 @@ class StackingNew:
         return final_prediction
 
     def get_name(self):
-        name = "stacking_new_" + self.meta_learner_type
+        name = "stacking_new_" + self.meta_learner_type + "_" + self.type
         if self.cross_validation:
             name = name + "_cross_validation"
         if self.feature_selection is not None:
--- survival_tests/ensembles/stacking_pre_computed.py
+++ survival_tests/ensembles/stacking_pre_computed.py
@@ -18,8 +18,8 @@ import sys
 
 class StackingPreComputed(StackingNew):
 
-    def __init__(self, meta_learner_type='per_algorithm_regressor', cross_validation=False, feature_selection=None, base_learner=None):
-        StackingNew.__init__(self, meta_learner_type='per_algorithm_regressor', cross_validation=False, feature_selection=None, base_learner=base_learner)
+    def __init__(self, meta_learner_type='random_forest_classifier', cross_validation=False, feature_selection=None, base_learner=None):
+        StackingNew.__init__(self, meta_learner_type=meta_learner_type, cross_validation=False, feature_selection=None, base_learner=base_learner)
 
     def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
         if 1 in self.base_learner:
@@ -37,22 +37,7 @@ class StackingPreComputed(StackingNew):
         if 7 in self.base_learner:
             self.base_learners.append(self.open_base_learner('multiclass', scenario.scenario, fold))
 
-        self.num_algorithms = len(scenario.algorithms)
-        feature_data = scenario.feature_data.to_numpy()
-        num_instances = len(feature_data)
-        x_train = np.zeros((num_instances, self.num_algorithms))
-
-        for i, base_learner in enumerate(self.base_learners):
-            for instance_number, x_test in enumerate(feature_data):
-                algorithm_prediction = np.argmin(base_learner.predict(x_test, instance_number))
-                x_train[instance_number][algorithm_prediction] = x_train[instance_number][algorithm_prediction] + 1
-
-        self.meta_learner = RandomForestClassifier(n_jobs=1, n_estimators=100)
-        self.meta_learner.set_params(random_state=fold)
-        y_train = list()
-        for data in scenario.performance_data.to_numpy():
-            y_train.append(np.argmin(data))
-        self.meta_learner.fit(x_train, y_train)
+        StackingNew.fit(scenario, fold, amount_of_training_instances, pre_computed=True)
 
     def predict(self, features_of_test_instance, instance_id: int):
         return StackingNew.predict(self, features_of_test_instance, instance_id)
