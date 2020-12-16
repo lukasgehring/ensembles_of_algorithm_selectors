3a04ddd14efa5c732e26bcbc6aa3734e1131f2ee
Lukas Gehring <lgehring@mail.upb.de>
2020-12-16 12:23:42 +0100

    Add AdaBoost.R2 working version

--- survival_tests/ensembles/adaboost_r2.py
+++ survival_tests/ensembles/adaboost_r2.py
@@ -1,43 +1,43 @@
+import copy
 import logging
 import sys
 import math
-
 import numpy as np
-from scipy.stats import rankdata
-from sklearn.utils import resample
 
 from approaches.survival_forests.surrogate import SurrogateSurvivalForest
 from baselines.multiclass_algorithm_selector import MultiClassAlgorithmSelector
 from baselines.per_algorithm_regressor import PerAlgorithmRegressor
 from aslib_scenario.aslib_scenario import ASlibScenario
-from math import log, exp
-
-from number_unsolved_instances import NumberUnsolvedInstances
 
 
 class AdaboostR2:
 
-    def __init__(self, algorithm_name, num_iterations=10):
-        self.algorithm_name = algorithm_name
-        self.num_iterations = num_iterations
+    def __init__(self, algorithm_name, max_iterations=20):
+        # setup
         self.logger = logging.getLogger("boosting")
         self.logger.addHandler(logging.StreamHandler())
-        self.num_algorithms = 0
-        self.num_models = 0
+
+        # parameters
+        self.algorithm_name = algorithm_name
+        self.max_iterations = max_iterations
+
+        # attributes
+        self.current_iteration = 1
         self.base_learners = list()
         self.beta = list()
         self.data_weights = list()
-        self.metric = NumberUnsolvedInstances(False)
-        self.performances = list()
 
     def fit(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
+        # setup values
         actual_num_training_instances = amount_of_training_instances if amount_of_training_instances != -1 else len(scenario.instances)
-        self.num_algorithms = len(scenario.algorithms)
-        self.data_weights = np.ones(actual_num_training_instances) / actual_num_training_instances
+        self.data_weights = np.ones(actual_num_training_instances)
 
-        for iteration in range(self.num_iterations):
+        # boosting iterations (stop when avg_loss >= 0.5 or iteration = max_iterations)
+        for iteration in range(self.max_iterations):
+
+            # choose base learner algorithm
             if self.algorithm_name == 'per_algorithm_regressor':
-                self.base_learners.append(PerAlgorithmRegressor(data_weights=self.data_weights))
+                self.base_learners.append(PerAlgorithmRegressor())
             elif self.algorithm_name == 'multiclass_algorithm_selector':
                 self.base_learners.append(MultiClassAlgorithmSelector(data_weights=self.data_weights))
             elif self.algorithm_name == 'ExponentialSurvivalForest':
@@ -45,20 +45,33 @@ class AdaboostR2:
             else:
                 sys.exit('Wrong base learner for boosting')
 
-            self.base_learners[iteration].fit(scenario, fold, amount_of_training_instances)
-            self.update_weights(scenario, self.base_learners[iteration], actual_num_training_instances)
+            # get weighted scenario and train new base learner
+            new_scenario = self.generate_weighted_sample(scenario, fold, actual_num_training_instances)
+            self.base_learners[iteration].fit(new_scenario, fold, amount_of_training_instances)
+
+            # calculate weights for next iteration
+            if not self.update_weights(scenario, self.base_learners[iteration], actual_num_training_instances):
+                break
+
+            self.current_iteration = self.current_iteration + 1
+            # use write_to_database to write each iteration to database
+            #write_to_database(scenario, self, fold)
 
     def predict(self, features_of_test_instance, instance_id: int):
+        # get the predictions and confidence values from each base learner
         y_predictions = list()
         for base_learner, beta in zip(self.base_learners, self.beta):
             y_predictions.append((np.amin(base_learner.predict(features_of_test_instance, instance_id)), beta, base_learner))
+        # sort base learners based on prediction value
         y_predictions.sort(key=lambda x: x[0])
 
+        # set lower_bound for prediction
         lower_bound = 0.0
         for beta in y_predictions:
             lower_bound = lower_bound + math.log(1/beta[1])
         lower_bound = lower_bound * 0.5
 
+        # sum beta while beta_sum < lower_bound -> this is the weighted median
         beta_sum = 0.0
         for t, beta in enumerate(y_predictions):
             beta_sum = beta_sum + math.log(1 / beta[1])
@@ -67,6 +80,7 @@ class AdaboostR2:
                 return prediction
 
     def update_weights(self, scenario: ASlibScenario, base_learner, amount_of_training_instances: int):
+        # get data from original scenario
         feature_data = scenario.feature_data.to_numpy()
         performance_data = scenario.performance_data.to_numpy()
         feature_cost_data = scenario.feature_cost_data.to_numpy() if scenario.feature_cost_data is not None else None
@@ -74,6 +88,7 @@ class AdaboostR2:
         temp_loss = list()
 
         for instance_id in range(amount_of_training_instances):
+            # all instances are for testing
             x_test = feature_data[instance_id]
             y_test = performance_data[instance_id]
 
@@ -89,15 +104,41 @@ class AdaboostR2:
             # if contains_non_censored_value:
             #    num_counted_test_values += 1
 
+            # calculate loss function for each instance
             predictions = base_learner.predict(x_test, instance_id)
             index = np.argmin(y_test)
             temp_loss.append(abs(predictions.flatten()[index] - y_test[index]))
+
+        # calculate loss function for base learner
         loss = temp_loss / np.amax(temp_loss)
-        avg_loss = sum(loss * self.data_weights)
+
+        # calculate average loss
+        avg_loss = sum(loss * (self.data_weights / sum(self.data_weights)))
+
+        # calculate confidence -> lower = better
         beta = avg_loss / (1 - avg_loss)
         self.beta.append(beta)
+
+        # update weights
         self.data_weights = self.data_weights * beta**(1-loss)
 
+        # check if average loss function is below 0.5
+        if avg_loss >= 0.5:
+            return False
+        return True
+
+    def generate_weighted_sample(self, scenario: ASlibScenario, fold: int, amount_of_training_instances: int):
+        # copy original scenario
+        new_scenario = copy.deepcopy(scenario)
+
+        # create weighted sample
+        new_scenario.feature_data = scenario.feature_data.sample(amount_of_training_instances, replace=True, weights=self.data_weights, random_state=fold)
+        new_scenario.performance_data = scenario.performance_data.sample(amount_of_training_instances, replace=True, weights=self.data_weights, random_state=fold)
+        if scenario.feature_cost_data is not None:
+            new_scenario.feature_cost_data = scenario.feature_cost_data.sample(amount_of_training_instances, replace=True, weights=self.data_weights, random_state=fold)
+
+        return new_scenario
+
     def get_name(self):
-        name = "adaboostR2_" + self.algorithm_name + "_" + str(self.num_iterations)
+        name = "adaboostR2_" + self.algorithm_name + "_" + str(self.current_iteration)
         return name
