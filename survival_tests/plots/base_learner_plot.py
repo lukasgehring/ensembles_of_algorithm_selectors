663eae8ce58a3a7e9504c04ffa2d75915d160979
Lukas Gehring <lgehring@mail.upb.de>
2021-01-16 16:55:04 +0100

    Update plots

--- survival_tests/plots/base_learner_plot.py
+++ survival_tests/plots/base_learner_plot.py
@@ -13,6 +13,9 @@ def load_configuration():
 def generate_sbs_vbs_change_table():
     color1 = '#264653'
     color2 = '#2a9d8f'
+    color3 = '#e76f51'
+    color4 = '#e9c46a'
+    color5 = '#251314'
 
     per_algorithm_regressor = get_dataframe_for_sql_query(
         "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='per_algorithm_RandomForestRegressor_regressor' GROUP BY approach")
@@ -29,49 +32,63 @@ def generate_sbs_vbs_change_table():
     par10 = get_dataframe_for_sql_query(
         "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='PAR10_algorithm_survival_forest' GROUP BY approach")
 
+    unsolved_instances = get_dataframe_for_sql_query(
+        "SELECT s.approach, AVG(s.unsolved_instances) as result FROM (SELECT scenario_name, AVG(result) as unsolved_instances, approach FROM `base_learner` WHERE metric='number_unsolved_instances_False' GROUP BY scenario_name, approach) as s GROUP BY s.approach ")
+
+
     plt.rc('font', family='sans-serif')
     plt.rc('text', usetex=True)
 
-    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+    fig = plt.figure(1, figsize=(7, 5))
+
+    ax1 = fig.add_subplot(111)
 
     base_learner = list()
     base_learner.append(float(per_algorithm_regressor.result))
-    base_learner.append(float(satzilla.result))
-    base_learner.append(float(expectation.result))
     base_learner.append(float(sunny.result))
     base_learner.append(float(isac.result))
-    base_learner.append(float(multiclass.result))
+    base_learner.append(float(satzilla.result))
+    base_learner.append(float(expectation.result))
     base_learner.append(float(par10.result))
+    base_learner.append(float(multiclass.result))
 
     names = list()
-    names.append("PerAlgorithmRegressor")
-    names.append("SatZilla-11")
-    names.append("Survival Forest Expectation")
+    names.append("PerAlgo")
     names.append("SUNNY")
     names.append("ISAC")
-    names.append("Multiclass Algorithm Selector")
-    names.append("Survival Forest PAR10")
+    names.append("SatZilla")
+    names.append("SF-Expectation")
+    names.append("SF-PAR10")
+    names.append("Multiclass")
 
     width = 0.5  # the width of the bars
     ind = np.arange(len(names))
-    ax.bar(ind, base_learner, width, color=color2, label='Single Learner')
+    ax1.bar(ind, base_learner, width, color=color1, label='Single Learner')
 
-    ax.set_xticks(ind)
-    ax.set_xticklabels(names)
+    ax1.set_xticks(ind)
+    ax1.set_xticklabels(names)
+    ax1.tick_params(axis='y', colors=color1)
 
     for i, value in enumerate(base_learner):
-        ax.text(i, value, round(value, 2), ha='center', va='bottom', rotation=0)
+        ax1.text(i, value, round(value, 2), ha='center', va='bottom', rotation=0)
 
 
     plt.xlabel('Learing Algorithm')
     plt.ylabel('nPAR10')
 
-    ax.set_ylim(bottom=0.35)
-    plt.xticks(rotation=90)
-    #plt.legend()
+    ax1.set_ylim(bottom=0)
+    ax1.set_ylim(top=1)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.set_ylabel('unsolved instances (\%)')
+    ax2.plot(ind, unsolved_instances.result, marker='s', lw=0, color=color3)
+    ax2.set_yticks(np.arange(6) / 25)
+    ax2.tick_params(axis='y', colors=color3)
+
+    plt.grid(b=True, which='major', linestyle='-')
     plt.show()
 
-    #fig.savefig("foo.pdf", bbox_inches='tight')
+    fig.savefig("plotted/baselines.pdf", bbox_inches='tight')
 
 
 def get_dataframe_for_sql_query(sql_query: str):
--- survival_tests/plots/voting_plot.py
+++ survival_tests/plots/voting_plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import configparser
 from matplotlib import pyplot as plt
+import matplotlib.patches as mpatches
 import numpy as np
 
 
@@ -11,79 +12,56 @@ def load_configuration():
 
 
 def generate_sbs_vbs_change_table():
-    color1 = '#2a9d8f'
-    color2 = '#e9c46a'
+    color1 = '#264653'
+    color2 = '#2a9d8f'
     color3 = '#e76f51'
-    color4 = '#264653'
+    color4 = '#e9c46a'
     color5 = '#251314'
 
     voting1234567 = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, voting.approach, vbs_sbs.metric, voting.result, ((voting.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN voting ON vbs_sbs.scenario_name = voting.scenario_name AND vbs_sbs.fold = voting.fold AND vbs_sbs.metric = voting.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='voting_1_2_3_4_5_6_7_pre' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY approach")
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, voting.approach, vbs_sbs.metric, voting.result, ((voting.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN voting ON vbs_sbs.scenario_name = voting.scenario_name AND vbs_sbs.fold = voting.fold AND vbs_sbs.metric = voting.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND approach='voting_1_2_3_4_5_6_7_pre' AND NOT scenario_name='CSP-Minizinc-Obj-2016' GROUP BY scenario_name")
 
-    per_algorithm_regressor = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='per_algorithm_RandomForestRegressor_regressor' GROUP BY approach")
     satzilla = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='satzilla-11' GROUP BY approach")
-    expectation = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='Expectation_algorithm_survival_forest' GROUP BY approach")
-    par10 = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='PAR10_algorithm_survival_forest' GROUP BY approach")
-    sunny = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='sunny' GROUP BY approach")
-    isac = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='isac' GROUP BY approach")
-    multiclass = get_dataframe_for_sql_query(
-        "SELECT approach, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='multiclass_algorithm_selector' GROUP BY approach")
+        "SELECT scenario_name, AVG(n_par10) as result FROM (SELECT vbs_sbs.scenario_name, vbs_sbs.fold, base_learner.approach, vbs_sbs.metric, base_learner.result, ((base_learner.result - vbs_sbs.oracle_result)/(vbs_sbs.sbs_result -vbs_sbs.oracle_result)) as n_par10,vbs_sbs.oracle_result, vbs_sbs.sbs_result FROM (SELECT oracle_table.scenario_name, oracle_table.fold, oracle_table.metric, oracle_result, sbs_result FROM (SELECT scenario_name, fold, approach, metric, result as oracle_result FROM `vbs_sbs` WHERE approach='oracle') as oracle_table JOIN (SELECT scenario_name, fold, approach, metric, result as sbs_result FROM `vbs_sbs` WHERE approach='sbs') as sbs_table ON oracle_table.scenario_name = sbs_table.scenario_name AND oracle_table.fold=sbs_table.fold AND oracle_table.metric = sbs_table.metric) as vbs_sbs JOIN base_learner ON vbs_sbs.scenario_name = base_learner.scenario_name AND vbs_sbs.fold = base_learner.fold AND vbs_sbs.metric = base_learner.metric WHERE vbs_sbs.metric='par10') as final WHERE metric='par10' AND NOT scenario_name='CSP-Minizinc-Obj-2016' AND approach='satzilla-11' GROUP BY scenario_name")
 
-    fig, ax = plt.subplots()  # Create a figure containing a single axes.
+    print(satzilla)
 
-    width = 0.4  # the width of the bars
-    ax.bar(1, voting1234567.result, width, color=color1)
+    plt.rc('font', family='sans-serif')
+    plt.rc('text', usetex=True)
 
-    ax.bar(6, per_algorithm_regressor.result, width, color=color4)
+    fig = plt.figure(1, figsize=(10, 5))
 
-    ax.bar(5, sunny.result, width, color=color4)
+    ax = fig.add_subplot(111)
 
-    ax.bar(8, isac.result, width, color=color4)
+    width = 0.3  # the width of the bars
+    ind = np.arange(13)
+    ax.bar(ind - width / 2, satzilla.result, width, color=color1, zorder=6)
+    ax.bar(ind + width / 2, voting1234567.result, width, color=color3, zorder=6)
 
-    ax.bar(2, satzilla.result, width, color=color4)
+    plt.axhline(np.average(satzilla.result), color=color1, linestyle='dashed', linewidth=2, zorder=3)
+    plt.axhline(np.average(voting1234567.result), color=color3, linestyle='dashed', linewidth=2, zorder=3)
 
-    ax.bar(3, expectation.result, width, color=color4)
+    ax.set_xticks(ind)
+    ax.set_xticklabels(["ASP-POTASSCO", "BNSL-2016", "CPMP-2015", "CSP-2010", "CSP-Minizinc-Time-2016", "CSP-MZN-2013", "GLUHACK-18", "MAXSAT12-PMS", "MAXSAT15-PMS-INDU", "QBF-2011", "SAT03-16\_INDU", "SAT12-INDU", "SAT18-EXP"])
 
-    ax.bar(4, par10.result, width, color=color4)
-
-    ax.bar(7, multiclass.result, width, color=color4)
-
-    ax.set_xticks([1, 6, 5, 8, 2, 3, 4, 7])
-    ax.set_xticklabels(["Voting", "PerAlgorithmRegressor", "SUNNY", "ISAC", "SATzilla'11", "SurvivalForestExpectation", "SurvivalForestPAR10", "Multiclass"])
-
-    ax.text(1, float(voting1234567.result), round(float(voting1234567.result), 3), ha='center',
-            va='bottom', rotation=0)
-    ax.text(6, float(per_algorithm_regressor.result), round(float(per_algorithm_regressor.result), 3), ha='center',
-            va='bottom', rotation=0)
-    ax.text(5, float(sunny.result), round(float(sunny.result), 3),
-            ha='center', va='bottom', rotation=0)
-    ax.text(8, float(isac.result), round(float(isac.result), 3),
-            ha='center', va='bottom', rotation=0)
-    ax.text(2, float(satzilla.result),
-            round(float(satzilla.result), 3), ha='center', va='bottom', rotation=0)
-    ax.text(3, float(expectation.result),
-            round(float(expectation.result), 3), ha='center', va='bottom', rotation=0)
-    ax.text(4, float(par10.result),
-            round(float(par10.result), 3), ha='center', va='bottom', rotation=0)
-    ax.text(7, float(multiclass.result),
-            round(float(multiclass.result), 3), ha='center', va='bottom', rotation=0)
 
     plt.xticks(rotation=45, ha='right')
 
-    ax.set_ylabel('nPAR10')
+    ax.set_ylabel('nPAR10', fontsize=11)
 
-    ax.set_ylim(bottom=0.3)
-    ax.set_ylim(top=0.75)
+    #ax.set_ylim(bottom=0.3)
+    #ax.set_ylim(top=0.7)
 
+    plt.grid(b=True, which='major', linestyle='-', axis='y', zorder=0)
+
+    l1 = mpatches.Patch(color=color1, label="SATzilla'11")
+    l2 = mpatches.Patch(color=color3, label="Voting")
+
+    fig.legend(handles=[l1, l2], loc=1, prop={'size': 13}, bbox_to_anchor=(0.99, 0.98))
+    plt.legend()
     plt.show()
 
-    fig.savefig("foo.pdf", bbox_inches='tight')
+    fig.savefig("plotted/voting_vs_baseline.pdf", bbox_inches='tight')
 
 
 def get_dataframe_for_sql_query(sql_query: str):
